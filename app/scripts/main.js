e77b92b14ac33918077a6ba1e8476bbac163ba7d
Maxime Musin <hello@maximemusin.me>
2017-02-25 19:11:07 +0100

    contact & about page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,56 @@
 
+window.onload = function () {
+  initMap();
+
+  function initMap() {
+    var styles = [
+      {
+        stylers: [
+          { hue: "#00ffe6" },
+          { saturation: -20 }
+        ]
+      },{
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          { lightness: 100 },
+          { visibility: "simplified" }
+        ]
+      },{
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      }
+    ];
+
+    var styledMap = new google.maps.StyledMapType(styles,
+      {name: "Styled Map"});
+    var myLatlng = new google.maps.LatLng(50.833511, 4.358177);
+
+    var mapOptions = {
+      zoom: 13,
+      center: myLatlng,
+      disableDefaultUI: true,
+      mapTypeControlOptions: {
+        mapTypeIds: ['map_style']
+      }
+    };
+    var map = new google.maps.Map(document.getElementById('map_office'),
+      mapOptions);
+
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+
+    var image = '../images/marker.svg';
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      title:"Our Office !",
+      icon: image
+    });
+
+    // To add the marker to the map, call setMap();
+    marker.setMap(map);
+  }
+};
